94f97b251aecd412c4b40855d817a8e0ad29571a
Trent <devtrentoh@gmail.com>
2021-08-30 18:59:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ st.markdown('''
 
 Some superheroes are Good and some are Evil. How does this happen? What kind of internal distribution exists for Good 
 and Evil aligned superheroes? In this ML-powered Visualization we investigate characteristics of Good and Evil Superheroes.
-You can even predict whether your custom Superhero aligns more with Good vs. Evil.
+You can predict whether your custom Superhero aligns more with Good vs. Evil. Gender prediction is also provided.
+Try creating your own Superhero!
 ''')
 
 if menu == "Good vs Evil Infographic":
